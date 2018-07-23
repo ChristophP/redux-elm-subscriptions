38b5d1ab073ec93f17afa2022e0391f740df2c5f
ChristophP <deedop@hotmail.de>
2018-07-23 22:53:00 +0200

    brush up example

--- example.js
+++ example.js
@@ -1,32 +1,34 @@
-// it needs to return an unsubscribe function
-const getArrowKeySub = dispatch => {
-  const handler = e => {
-    if (e.keyCode === '... ') {
-      dispatch({ type: 'ARROW_PRESS', payload: 'right' });
-    }
-    if (e.keyCode === '...') {
-      dispatch({ type: 'ARROW_PRESS', payload: 'left' });
-    }
-  };
-  document.addEventListener('keydown', handler);
-  return () => {
-    document.removeEventListener('keydown', handler);
-  };
-};
+import { createSubscriptions, subNone } from 'redux-elm-subscriptions';
 
 const subs = (state, dispatch) => {
-  const arrowKeys =
-    state.page === 'page I wanna subscribe on' ? arrowKeysSub : subNone;
+  const listenToDocClicks = () => {
+    const handler = () => {
+      dispatch({ type: 'DOCUMENT_CLICK' });
+    };
+    document.addEventListener('click', handler);
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  };
 
   return {
-    arrowKeys: getArrowKeySub(state, dispatch),
-    anotherSubscriptions: () => {
-      // subscribe
+    // it needs to return an unsubscribe function
+    arrowKeys: () => {
+      const handler = e => {
+        if (e.keyCode === 39) {
+          dispatch({ type: 'ARROW_PRESS', payload: 'right' });
+        } else if (e.keyCode === 37) {
+          dispatch({ type: 'ARROW_PRESS', payload: 'left' });
+        }
+      };
+      document.addEventListener('keydown', handler);
       return () => {
-        // unsubscribe
+        document.removeEventListener('keydown', handler);
       };
     },
+    // subscribe when modal is open, unsubscribe when it's closed
+    documentClick: state.modalIsOpen ? listenToDocClick : subNone,
   };
 };
 
-store.subscribe(createSubscriptions(store)());
+store.subscribe(createSubscriptions(store)(subs));
