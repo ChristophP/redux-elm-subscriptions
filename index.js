2099db8a6ad50906e8bee118151ab9ade7dd93c6
ChristophP <deedop@hotmail.de>
2018-09-22 20:07:46 +0200

    remove use of arrow functions

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 module.exports.subNone = null;
 
-module.exports.createSubscriptions = ({
-  getState,
-  dispatch,
-}) => mapStateToSubs => {
-  var _subState = {};
+module.exports.createSubscriptions = function(store) {
+  return function(mapStateToSubs) {
+    var _subState = {};
 
-  return function() {
-    var state = getState();
-    var subs = mapStateToSubs(state, dispatch);
-    // if new value is there, subscribe and store unsubscribe function
-    Object.keys(subs).forEach(function(key) {
-      if (!_subState[key] && typeof subs[key] === 'function') {
-        _subState[key] = subs[key](state, dispatch);
-      }
-    });
+    return function() {
+      var state = store.getState();
+      var subs = mapStateToSubs(state, store.dispatch);
+      // if new value is there, subscribe and store unsubscribe function
+      Object.keys(subs).forEach(function(key) {
+        if (!_subState[key] && typeof subs[key] === 'function') {
+          _subState[key] = subs[key](state, store.dispatch);
+        }
+      });
 
-    // if value is removed, unsubscribe
-    Object.keys(_subState).forEach(function(key) {
-      if (!subs[key] && typeof _subState[key] === 'function') {
-        _subState[key]();
-        delete _subState[key];
-      }
-    });
+      // if value is removed, unsubscribe
+      Object.keys(_subState).forEach(function(key) {
+        if (!subs[key] && typeof _subState[key] === 'function') {
+          _subState[key]();
+          delete _subState[key];
+        }
+      });
+    };
   };
 };
